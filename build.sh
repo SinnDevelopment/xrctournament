c8efe61ade9f828588d3bed3c1a891570cd2585b
Jamie Sinn <james.sinn@sinndevelopment.com>
2020-10-30 14:09:28 -0400

    Add stripping back in, including cgo too as a test

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ filename="xrctournament_$version$extension"
 
 if [ "$version" == "pro" ]
 then
-    go build -v -tags pro -o "$filename"
+    go build -v -tags pro -ldflags "-s -w" -o "$filename"
 else
-    go build -v -tags free -o "$filename"
+    go build -v -tags free -ldflags "-s -w" -o "$filename"
 fi
